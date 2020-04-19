e13f8816db45ead2159d5e2756b7a808260b729f
konrad151 <konrad.k151@gmail.com>
2020-04-20 01:55:28 +0200

    Create header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap'
+import styles from './Header.module.scss'
 
 const Header = () => {
 	return (
-		<div>
-			Header
-		</div>
-	);
-};
+		<header className={styles.header}>
+			<Container>
+				<Row>
+					<Col xs={12} sm={'auto'}>
+						<h1 className={styles.header__logo}>Team Generator</h1>
+					</Col>
+					<Col xs={12} sm>
+						<nav className={styles.header__nav}>
+							<Link to="/" className={styles.header__navItem}>Generator</Link>
+							<Link to="/teams" className={styles.header__navItem}>Teams</Link>
+						</nav>
+					</Col>
+				</Row>
+			</Container>
+		</header>
+	)
+}
 
-export default Header;
+export default Header
