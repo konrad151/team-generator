87737e89a1e51bf1403f8c2a5350571c5cca4263
konrad151 <konrad.k151@gmail.com>
2020-04-20 01:44:20 +0200

    Install react-bootstrap, bootstrap and node-sass for scss styling

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
 
+import './scss/index.scss'
+
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<App />,
 	document.getElementById('root')
 )
